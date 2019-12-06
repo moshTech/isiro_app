ace54d604a3188074f2771a5f86349718d780748
moshTech <mosh.onaarajnr@gmail.com>
2019-12-06 08:25:08 +0100

    change the splash duration

--- lib/splash.dart
+++ lib/splash.dart
@@ -22,7 +22,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   Future<bool> _mockCheckForSession() async {
-    await Future.delayed(Duration(seconds: 5), () {});
+    await Future.delayed(Duration(seconds: 15), () {});
 
     return true;
   }
